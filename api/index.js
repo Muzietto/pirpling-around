01290dbd349befa9dfa95c00517981289eb37664
Marco Faustinelli (Muzietto) <marco_faustinelli@yahoo.com>
2018-10-09 03:58:03 +0200

    parsing method and QS

--- api/index.js
+++ api/index.js
@@ -5,12 +5,17 @@ var url = require('url');
 var server = http.createServer(function(req, res) {
 
   var parsedUrl = url.parse(req.url, true);
+
   var path = parsedUrl.pathname; // /pippo/gino//
   var trimmedPath = path.replace(/^\/+|\/+$/g, ''); // pippo/gino
 
+  var qs = parsedUrl.query;
+
+  var method = req.method.toLowerCase();
+
   res.end('Hello, World!\n');
 
-  console.log('Request received on /' + trimmedPath);
+  console.log(`Request received on /${trimmedPath} using method ${method} and qs ${JSON.stringify(qs)}`);
 });
 
 server.listen(3000, function() {
