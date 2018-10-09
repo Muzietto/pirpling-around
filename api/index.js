27bf2736b6564e26bd2a5b3f34c62c9b97dd43f6
Marco Faustinelli (Muzietto) <marco_faustinelli@yahoo.com>
2018-10-09 03:36:09 +0200

    extract trimmed paths

--- api/index.js
+++ api/index.js
@@ -1,8 +1,16 @@
 
 var http = require('http');
+var url = require('url');
 
 var server = http.createServer(function(req, res) {
-  res.end('Hello, World!');
+
+  var parsedUrl = url.parse(req.url, true);
+  var path = parsedUrl.pathname; // /pippo/gino//
+  var trimmedPath = path.replace(/^\/+|\/+$/g, ''); // pippo/gino
+
+  res.end('Hello, World!\n');
+
+  console.log('Request received on /' + trimmedPath);
 });
 
 server.listen(3000, function() {
