c1b4eb9c5bf892615686d64429afc59d80a7f0f6
Marco Faustinelli (Muzietto) <marco_faustinelli@yahoo.com>
2018-11-13 14:38:22 +0100

    promise-based datasource

--- api/lib/data0.js
+++ api/lib/data0.js
@@ -8,70 +8,41 @@ var lib = {};
 
 lib.baseDir = path.join(__dirname, '/../.data/');
 
-// TODO complete it, so that it returns a promise
-lib.create = (dir, fileName, data, callback) => {
+lib.create = (dir, fileName, data) => {
   var _fileName = lib.baseDir + dir + '/' + fileName + '.json';
 
   return fs.open(_fileName, 'wx')
-    .then(file => {
-      var _data = JSON.stringify(data);
-
-      return fs.writeFile(file, _data)
-        .then(() => fs.close(file)
-            .then(() => { callback(false); })
-            .catch(err => { callback(`Error closing new file ${_fileName}: ${err}`); });
-        )
-        .catch(err => { callback(`Error writing to new file ${_fileName}: ${err}`); });
-    })
-    .catch(err => { callback(`Error opening file ${_fileName} for writing: ${err}`); });
+    .then(file => fs.writeFile(file, JSON.stringify(data))
+    .then(fs.close)
+    .then(_ => Promise.resolve(false))
+    .catch(err => Promise.reject(`Error creating file ${_fileName}: ${err}`));
 };
 
-// returns a promise
 lib.read = (dir, fileName) => {
   var _fileName = lib.baseDir + dir + '/' + fileName + '.json';
+
   return fs.readFile(_fileName, 'utf-8')
     .then(data => Promise.resolve(helpers.parseJsonToObject(data)))
-    .catch(err => Promise.reject(`Error reading user data: ${err}`)); // used to be an object {error:msg}
+    .catch(err => Promise.reject(`Error reading user data: ${err}`));
 };
 
-lib.update = (dir, fileName, data, callback) => {
+lib.update = (dir, fileName, data) => {
   var _fileName = lib.baseDir + dir + '/' + fileName + '.json';
 
-  fs.open(_fileName, 'r+', (err, file) => {
-    if (!err && file) {
-      var _data = JSON.stringify(data);
-
-      fs.ftruncate(file, err => {
-
-        if (!err) {
-
-          fs.writeFile(file, _data, err => {
-            if (!err) {
-
-              fs.close(file, err => {
-                callback((err) ? `Error closing updated file ${_fileName}: ${err}` : false);
-              });
-            } else {
-              callback(`Error updating file ${_fileName}: ${err}`);
-            }
-          });
-        } else {
-          callback(`Error truncating file ${_fileName}: ${err}`);
-        }
-      });
-
-    } else {
-      callback(`Error opening file ${_fileName} for updating: ${err}`);
-    }
-  });
+  return fs.open(_fileName, 'r+')
+    .then(fs.ftruncate)
+    .then(file => fs.writeFile(file, JSON.stringify(data)))
+    .then(fs.close)
+    .then(_ => Promise.resolve(false))
+    .catch(err => Promise.reject(`Error updating file ${_fileName}: ${err}`));
 };
 
 lib.delete = (dir, fileName, callback) => {
   var _fileName = lib.baseDir + dir + '/' + fileName + '.json';
 
-  fs.unlink(_fileName, err => {
-    callback((err) ? `Error deleting file ${_fileName}: ${err}` : false);
-  });
+  return fs.unlink(_fileName)
+    .then(_ => Promise.resolve(false))
+    .catch(err => Promise.reject(`Error deleting file ${_fileName}: ${err}`));
 };
 
 module.exports = lib;
