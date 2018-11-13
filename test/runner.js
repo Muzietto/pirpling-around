d896df4d857fc85260de6286637ccb94a5cd3376
Marco Faustinelli (Muzietto) <marco_faustinelli@yahoo.com>
2018-11-13 18:37:46 +0100

    test runner improved, but not totally async

--- test/runner.js
+++ test/runner.js
@@ -6,36 +6,50 @@ runner.tests = {
 };
 
 runner.run = _ => {
+  var delay = 0;
   var count = 0;
   var successes = 0;
   var errors = [];
+  var promises = [];
 
   Object.keys(runner.tests).forEach(type => {
     Object.keys(runner.tests[type]).forEach(testName => {
-      try {
-        runner.tests[type][testName](_ => {
-          successes++;
-        });
-      } catch(error) {
-        errors.push({error, testName});
-      } finally {
-        count++;
-      }
+      delay += 50;
+      (d => {
+        promises.push(new Promise((resolve, reject) => {
+          setTimeout(_ => {
+            try {
+              runner.tests[type][testName](_ => {
+                successes++;
+                count++;
+                resolve(count);
+              });
+            } catch(error) {
+              errors.push({error, testName});
+              count++;
+              resolve(count);
+            }
+          }, d);
+        }));
+      })(delay);
     });
   });
 
-  console.log('');
-  console.log('--------TEST REPORT-------');
-  console.log(`Ran: ${count}`);
-  console.log('\x1b[32m%s\x1b[0m', `Pass: ${successes}`);
-  console.log('\x1b[31m%s\x1b[0m', `Fail: ${errors.length}`);
+  Promise.all(promises)
+    .then(_ => {
+      console.log('');
+      console.log('--------TEST REPORT-------');
+      console.log(`Ran: ${count}`);
+      console.log('\x1b[32m%s\x1b[0m', `Pass: ${successes}`);
+      console.log('\x1b[31m%s\x1b[0m', `Fail: ${errors.length}`);
 
-  errors.forEach(error => {
-    console.log('');
-    console.log('\x1b[31m%s\x1b[0m', `Name: ${error.testName}, Error: ${error.error}`);
-  });
-  console.log('');
-  console.log('--------END TEST REPORT-------');
+      errors.forEach(error => {
+        console.log('');
+        console.log('\x1b[31m%s\x1b[0m', `Name: ${error.testName}, Error: ${error.error}`);
+      });
+      console.log('');
+      console.log('--------END TEST REPORT-------');
+    });
 };
 
 module.exports = runner;
