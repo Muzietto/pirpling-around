cd96e99af8395f38b7e7c3def43787917162e78a
Marco Faustinelli (Muzietto) <marco_faustinelli@yahoo.com>
2018-11-18 13:20:57 +0100

    completed users specs!!!

--- api/lib/handlers/users.spec.js
+++ api/lib/handlers/users.spec.js
@@ -3,13 +3,18 @@ var fs = require('fs');
 var assert = require('assert');
 var path = require('path');
 var dataHandlers = require('../data');
+var tokens = require('./tokens');
 var helpers = require('../helpers');
 
 var tests = {};
 var xtests = {};
 
-var phone = Date.now() + '';
-var baseDir = path.join(__dirname, '/../../.data/users');
+var userPhone = Date.now() + '';
+var tokensDir = path.join(__dirname, '/../../.data/tokens');
+var usersDir = path.join(__dirname, '/../../.data/users');
+var userPassword = `test ${userPhone}`;
+var userTokenId;
+var userTokenExpires;
 
 tests['users should be a callback-based interface'] = done => {
   assert.equal(users.interfaceType(), 'callback');
@@ -17,13 +22,12 @@ tests['users should be a callback-based interface'] = done => {
 };
 
 tests['users.post should create a user'] = done => {
-  var password = `test ${phone}`;
   var data = {
     payload: {
       firstName: 'firstName',
       lastName: 'lastName',
-      phone,
-      password,
+      phone: userPhone,
+      password: userPassword,
       tosAgreement: true,
     }
   };
@@ -31,53 +35,137 @@ tests['users.post should create a user'] = done => {
   users.post(data, (returnCode, error) => {
     assert.equal(returnCode, 200);
 
-    var userFilename = `${phone}.json`;
-    var foundUsers = fs.readdirSync(baseDir);
+    var userFilename = `${userPhone}.json`;
+    var foundUsers = fs.readdirSync(usersDir);
     assert.equal(foundUsers.includes(userFilename), true);
 
-    dataHandlers.read('users', phone, (returnCode, data) => {
+    dataHandlers.read('users', userPhone, (returnCode, data) => {
 
       assert.equal(returnCode, false);
       assert.equal(data.firstName, 'firstName');
       assert.equal(data.lastName, 'lastName');
-      assert.equal(data.phone, phone);
+      assert.equal(data.phone, userPhone);
       assert.equal(data.tosAgreement, true);
-      assert.equal(data.hashedPassword, helpers.hash(password));
+      assert.equal(data.hashedPassword, helpers.hash(userPassword));
       done();
     });
   });
 };
 
-xtests['users.get should read a user'] = done => {
-  var password = `test ${phone}`;
+tests['users.get should read a user, provided it gives the right token'] = done => {
+  // create a user token to begin with
   var data = {
-    queryStringObject: {
-      phone,
+    payload: {
+      phone: userPhone,
+      password: userPassword,
     }
   };
 
-  data.read('test', filename, (_, data) => {
-    assert.equal(JSON.stringify(data), jsonContent);
+  tokens.post(data, (returnCode, tokenObject) => {
+    userTokenId = tokenObject.id;
+    userTokenExpires = tokenObject.expires;
+
+    var getData = {
+      queryStringObject: {
+        phone: userPhone,
+      },
+      headers: {
+        token: userTokenId,
+      },
+    };
+
+    users.get(getData, (returnCode, data) => {
+      assert.equal(returnCode, 200);
+      assert.equal(data.firstName, 'firstName');
+      assert.equal(data.lastName, 'lastName');
+      assert.equal(data.phone, userPhone);
+      assert.equal(data.tosAgreement, true);
+      done();
+    });
+  });
+};
+
+tests['users.get shouldn\'t read a user, if the token is wrong'] = done => {
+  var getData = {
+    queryStringObject: {
+      phone: userPhone,
+    },
+    headers: {
+      token: 'wrongTokenId',
+    },
+  };
+
+  users.get(getData, (returnCode, data) => {
+    assert.equal(returnCode, 403);
+    assert.equal(data.error, 'missing token in header or invalid token');
     done();
   });
 };
 
-xtests['datasource should update a file'] = done => {
-  var text = `test ${filename} test`;
-  var jsonContent = JSON.stringify({ text });
+tests['users.get shouldn\'t read a user, if the token is missing'] = done => {
+  var getData = {
+    queryStringObject: {
+      phone: userPhone,
+    },
+    headers: {
+    },
+  };
 
-  data.update('test', filename, { text }, _ => {
-    var foundContent = fs.readFileSync(`${baseDir}/${filename}.json`);
-    assert.equal(foundContent, jsonContent);
+  users.get(getData, (returnCode, data) => {
+    assert.equal(returnCode, 403);
+    assert.equal(data.error, 'missing token in header or invalid token');
     done();
   });
 };
 
-xtests['datasource should delete a file'] = done => {
-  data.delete('test', filename, outcome => {
-    assert.equal(outcome, false);
-    var foundFiles = fs.readdirSync(baseDir);
-    assert.equal(foundFiles.length, 0);
+tests['users.put should update a user, provided it encloses the right token'] = done => {
+  var data = {
+    payload: {
+      phone: userPhone,
+      firstName: 'firstName2',
+      lastName: 'lastName2',
+      password: 'new_' + userPassword,
+    },
+    headers: {
+      token: userTokenId,
+    },
+  };
+
+  users.put(data, returnCode => {
+    assert.equal(returnCode, 200);
+    var getData = {
+      queryStringObject: {
+        phone: userPhone,
+      },
+      headers: {
+        token: userTokenId,
+      },
+    };
+
+    users.get(getData, (_, data) => {
+      assert.equal(data.firstName, 'firstName2');
+      assert.equal(data.lastName, 'lastName2');
+      done();
+    });
+  });
+};
+
+tests['users.delete should delete a user and, in the future, all his tokens'] = done => {
+  var data = {
+    payload: {
+      phone: userPhone,
+    },
+    headers: {
+      token: userTokenId,
+    },
+  };
+  var previousUsers = fs.readdirSync(usersDir);
+
+  users.delete(data, returnCode => {
+    assert.equal(returnCode, 200);
+    var currentUsers = fs.readdirSync(usersDir);
+    assert.equal(currentUsers.length, previousUsers.length - 1);
+    // TODO - check user tokens are gone
     done();
   });
 };
